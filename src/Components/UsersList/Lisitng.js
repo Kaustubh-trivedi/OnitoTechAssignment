3319d1d1679aa5619f4e30f659b64914f15b1bfc
Kaustubh-trivedi <kaustubh@alobhadev.com>
2024-02-26 00:17:59 +0530

    datatable implementation progress

--- src/Components/UsersList/Lisitng.js
+++ src/Components/UsersList/Lisitng.js
@@ -1,19 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getDataList } from '../../Redux/action/ApplicationAction';
+import $ from 'jquery';
+import 'datatables.net';
 
 const Lisitng = () => {
     const dispatch = useDispatch();
+    const tableRef = useRef(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getDataList());
-    },[])
+        $(tableRef.current).DataTable();
+    }, [])
 
-    const data = useSelector(state=> state.ApplicationReducer.dataList);
+    const data = useSelector(state => state.ApplicationReducer.dataList);
     console.log(data);
     return (
         <>
-
+            <table>
+                <thead>
+                    <tr>
+                        <th>S.No.</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Sex</th>
+                        <th>Mobile</th>
+                        <th>Govt Issued ID type</th>
+                        <th>Govt ID number</th>
+                    </tr>
+                </thead>
+            </table>
         </>
     )
 }
